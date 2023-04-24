5239bc4d9719ecc60a9ba1fd31941140b89a8487
lucas19fonseca <126473218+lucas19fonseca@users.noreply.github.com>
2023-04-24 10:42:50 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -13,7 +13,23 @@ package q1
 //A função deve retornar um valor booleano, indicando se é possível ou não dividir a melancia da forma desejada. Se o peso
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
 
-func DivideWatermelon(weight int) (bool, error) {
-	// Seu código aqui
-	return false, nil
+package main
+
+import (
+	"fmt"
+)
+
+func DivideWatermelon(PesoMelancia int) (bool, error) {
+	if PesoMelancia <= 0 {
+		return false, fmt.Errorf("peso inválido")
+	} else if PesoMelancia%2 != 0 {
+		return false, fmt.Errorf("peso inválido")
+	} else if PesoMelancia == 2 {
+		return false, nil
+	}
+	return true, nil
 }
+func main() {
+
+	possible, err := DivideWatermelon(10)
+	fmt.Printf("Q1:\tpossible:%v, \terr:: %v\n", possible, err)
